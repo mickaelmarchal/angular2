357455087f45b08ffae0ed41a876514204017eb3
Mickael Marchal <dev@snapsha.re>
2016-07-07 23:26:41 +0200

    finished chapter 5 of angular tutorial

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+
 @Injectable()
 export class HeroService {
+
   getHeroes() {
     return Promise.resolve(HEROES);
   }
@@ -11,5 +13,10 @@ export class HeroService {
     return new Promise<Hero[]>(resolve =>
       setTimeout(() => resolve(HEROES), 2000) // 2 seconds
     );
+  }
+
+  getHero(id: number) {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
