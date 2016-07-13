10cae62a619de15d2c53594989b96b44720e4e0d
Mickael Marchal <dev@snapsha.re>
2016-07-13 22:49:46 +0200

    finished chapter 7

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -3,16 +3,20 @@ import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
   styleUrls: ['app/heroes.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
   selectedHero: Hero;
+  error: any;
+  addingHero = false;
 
   constructor(
     private router: Router,
@@ -34,5 +38,26 @@ export class HeroesComponent implements OnInit {
 
   gotoDetail() {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  addHero() {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
+
+  close(savedHero: Hero) {
+    this.addingHero = false;
+    if (savedHero) { this.getHeroes(); }
+  }
+
+  deleteHero(hero: Hero, event: any) {
+    event.stopPropagation();
+    this.heroService
+      .delete(hero)
+      .then(res => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      })
+      .catch(error => this.error = error);
   }
 }
